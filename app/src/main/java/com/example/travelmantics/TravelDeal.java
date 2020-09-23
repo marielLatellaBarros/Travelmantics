2d68d5184f97eeca6eb1e30ec7cb514f283cf63b
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-09-23 20:46:31 +0200

    Read data from database and show it in a list
    - Create a new empty activity => res/layout/activity_list // Add a TextView element and apply layout changes so it takes as much space as its container (with some margins)
    - Setup ListActivity class
       - add FirebaseDatabase and DatabaseReference objects and add ChildEventListener that will listen for changes to trigger database refresh
       - implement onChildAdded event listener // get the emulator element (TextView), create a travelDeal object and populate it with the data that comes from the database - as a snapshot. The getValue method serialises the data and converts it to a TravelDeal object.
       - get the current text, add a new line, and add the new travelDeal title
       - set listener to listen for this object (mChildEventListener)
    - Add a default constructor (otherwise listener doesn't work (Class com.example.travelmantics.TravelDeal does not define a no-argument constructor.)
    - To load the list first (and not the InsertActivity) the intent-filter in the manifest needs to be under .ListActivity
    - Test listener listens for new entries by manually adding a new entry in Firebase (Name : 1 // new entry // Name "title", value "Test!!!"
    DONE!

--- app/src/main/java/com/example/travelmantics/TravelDeal.java
+++ app/src/main/java/com/example/travelmantics/TravelDeal.java
@@ -7,6 +7,8 @@ public class TravelDeal {
     private String price;
     private String imageUrl;
 
+    public TravelDeal() { }
+
     public TravelDeal(String title, String description, String price, String imageUrl) {
         this.id = id;
         this.title = title;
