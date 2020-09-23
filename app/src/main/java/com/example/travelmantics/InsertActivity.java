a15d3874d6546ad5b9c3c05b2174027b99119900
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-09-23 17:10:28 +0200

    Writing to the Realtime Database
    - Create the Firebase objects needed to get a database instance and save deals under correct reference (traveldeals)
    - Create the text objects that will contain the input values (EditText)
    - Bind the EditText objects with the input fields by looking at the resource id
    - Add "click event": when the id of the selected item is the same as the id of save_menu:
       - save the deal (convert the actual values from the EditText object to strings, create a new TravelDeal object and push it to the database)
       - show a "toast" with "Deal Saved"
       - clean the input fields and set focus on title so a new entry can be added

--- app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -1,16 +1,46 @@
 package com.example.travelmantics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class InsertActivity extends AppCompatActivity {
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    EditText txtTitle;
+    EditText txtPrice;
+    EditText txtDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+
+        txtTitle = findViewById(R.id.txtTitle);
+        txtPrice = findViewById(R.id.txtPrice);
+        txtDescription = findViewById(R.id.txtDescription);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.save_menu) { // also: use switch generally for menus
+            saveDeal();
+            Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
+            clean();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -19,5 +49,21 @@ public class InsertActivity extends AppCompatActivity {
         inflater.inflate(R.menu.save_menu, menu);
 
         return true;
+    }
+
+    private void saveDeal() {
+        String title = txtTitle.getText().toString();
+        String price = txtPrice.getText().toString();
+        String description = txtDescription.getText().toString();
+
+        TravelDeal deal = new TravelDeal(title, description, price, "");
+        mDatabaseReference.push().setValue(deal);
+    }
+
+    private void clean() {
+        txtTitle.setText("");
+        txtPrice.setText("");
+        txtDescription.setText("");
+        txtTitle.requestFocus();
     }
 }
