bd48870fb919e51d453adbdb182a411e9f3ece9a
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-09-24 11:52:40 +0200

    Use a RecyclerView with Firebase to display a scrolling list of data
    - Replace the TextView (for testing if data comes along with the eventListener) and put in place a RecyclerView.
    A RecyclerView allows scrolling list of data.
    Each row of a RecyclerView contains a TextView (each entry is one element). For this we add a new element a TextView to show the title (LinearLayout)
    
    In order for the RecyclerView to work, dus, get data, it needs an adapter.
    The adapter sends data to the RecyclerView through an object called ViewHolder. The ViewHolder describes an item of the RecyclerView. In this app, it is a single row of data containing the travel deal information (it also contains info about its place in the RecyclerView)
    The adapter should subclass the ViewHolder to load the views, and then bind the data to the views.
    ViewHolders get cached to make scrolling smoother, because the findViewById method gets called only when an item is created, and NOT each time it's updated.
    
    Finally, we have the LayoutManager. We can use this one in order to decide how the items are displayed in a RecyclerView.

---
+++ app/src/main/java/com/example/travelmantics/DealAdapter.java
@@ -0,0 +1,103 @@
+package com.example.travelmantics;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class DealAdapter extends RecyclerView.Adapter<DealAdapter.DealViewHolder> {
+    private ArrayList<TravelDeal> deals;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildEventListener;
+
+    public DealAdapter() {
+        FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+        deals = FirebaseUtil.mDeals;
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                TravelDeal travelDeal = snapshot.getValue(TravelDeal.class);
+                Log.d("Deal: ", travelDeal.getTitle());
+                travelDeal.setId(snapshot.getKey());
+                deals.add(travelDeal);
+                notifyItemInserted(deals.size() -1);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildEventListener);
+
+    }
+
+    @NonNull
+    @Override
+    public DealViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View itemView = LayoutInflater.from(context).inflate(R.layout.rv_row, parent, false);
+
+        return new DealViewHolder((itemView));
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DealViewHolder holder, int position) {
+        TravelDeal deal = deals.get(position);
+        holder.bind(deal);
+    }
+
+    @Override
+    public int getItemCount() {
+        return deals.size();
+    }
+
+    public class DealViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle;
+
+        public DealViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+        }
+
+        public void bind(TravelDeal deal) {
+            tvTitle.setText(deal.getTitle());
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/travelmantics/ListActivity.java
+++ app/src/main/java/com/example/travelmantics/ListActivity.java
@@ -1,63 +1,25 @@
 package com.example.travelmantics;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.TextView;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
-    private ArrayList<TravelDeal> deals;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
-    private ChildEventListener mChildEventListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        FirebaseUtil.openFbReference("traveldeals");
-        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
-        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+        RecyclerView rvDeals = findViewById(R.id.rvDeals);
+        final DealAdapter adapter = new DealAdapter();
+        rvDeals.setAdapter(adapter);
 
-        mChildEventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                TextView tvDeals = findViewById(R.id.tvDeals);
-                TravelDeal travelDeal = snapshot.getValue(TravelDeal.class);
-                tvDeals.setText(String.format("%s\n%s", tvDeals.getText(), travelDeal.getTitle()));
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        };
-        mDatabaseReference.addChildEventListener(mChildEventListener);
+        LinearLayoutManager dealsLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvDeals.setLayoutManager(dealsLayoutManager);
     }
 }
