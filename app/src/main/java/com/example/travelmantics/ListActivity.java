f5cfedc7e970c4aa554058eda272de1508c90e57
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-09-24 21:10:06 +0200

    The New Layout
    - Add description and price fields
    - Add menu to create a new entry
    - Refactor DealActivity as it will not only contain insert method but also edit and delete

--- app/src/main/java/com/example/travelmantics/ListActivity.java
+++ app/src/main/java/com/example/travelmantics/ListActivity.java
@@ -1,10 +1,15 @@
 package com.example.travelmantics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -21,5 +26,24 @@ public class ListActivity extends AppCompatActivity {
         LinearLayoutManager dealsLayoutManager =
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         rvDeals.setLayoutManager(dealsLayoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_activity_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.insert_menu) { // also: use switch generally for menus
+            Intent intent = new Intent(this, DealActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
